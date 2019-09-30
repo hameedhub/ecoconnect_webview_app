70ea0190ac20e2fe092f3cc419f0422131950f86
Hameed Abdulrahaman <hameedabdulrahamann@gmail.com>
2019-09-30 13:33:08 +0100

    task-completed

---
+++ app/src/main/java/com/example/ecoconnect/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.ecoconnect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webView.loadUrl("https://ecoconnect.ng/login.html");
+        webView.setWebViewClient(new WebViewClient(){
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl){
+                webView.loadUrl("file:///android_asset/error.html");
+            }
+        });
+
+
+
+    }
+}
